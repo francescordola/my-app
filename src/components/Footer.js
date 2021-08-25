92a8449186b7c65cf10fc24e03f9c18c5177fe50
Francesco Cordola <francesco.cordola@outlook.com>
2021-08-25 16:16:23 +0200

    time + date Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,54 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 // CITY
 const Name = (props) => {
-    console.log('ççççç')
+    //console.log('city')
     const name = <h1>{props.name}</h1> 
     return name
 }
 
-//CLOCK
-const Time = () => {
-    const [ dateTime, setDateTime ] = useState(new Date()) 
-    const tick = () => { setDateTime(new Date()) }
-    useEffect(() => {
-        setInterval(() => tick(),1000);
-    }, [])
-  return <h2>{dateTime.toLocaleTimeString()}</h2>
+/*  
+dentro alla funzione con useState scrivo quello che succede in background
+quello che stampo lo gestisco al di fuori della funzione con gli Hooks
+*/
+
+// const DateFormatted = ({ date }) => <h2>{date.toLocaleDateString()}</h2>
+// const TimeFormatted = ({ date }) => <h2>{date.toLocaleTimeString()}</h2>
+
+// DATE + TIME
+const DateTime = () => {
+    //console.log('date')
+    const [ date, setDateTime ] = useState(new Date());
+
+    useEffect(() => { 
+        console.log ('setInterval')
+        
+        setInterval(() => {
+            console.log('setDateTime')
+            setDateTime(new Date())
+        }, 1000)
+        
+    }, []);
+
+    const time = <h2>{date.toLocaleTimeString()}</h2>
+    const data = <h2>{date.toLocaleDateString()}</h2>
+
+    return <>
+        {data}
+        {time}
+
+        {/* <DateFormatted date={date} /> 
+        <TimeFormatted date={date} /> */}
+        
+    </>
 } 
 
 export const Footer = () => {
-    return (<footer>
-        <Name name="City" />
-        <Time />
+    return (
+    <footer>
+        <Name name='City' />
+        <DateTime />
     </footer>)
 }
+
