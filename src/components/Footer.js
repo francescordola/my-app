ba5654e47a47b8f1c9a803ce8b97059f1aadff05
Francesco Cordola <francesco.cordola@outlook.com>
2021-08-25 11:07:52 +0200

    set Time with Hooks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+// CITY
+const Name = (props) => {
+    console.log('ççççç')
+    const name = <h1>{props.name}</h1> 
+    return name
+}
+
+//CLOCK
+const Time = () => {
+    const [ dateTime, setDateTime ] = useState(new Date()) 
+    const tick = () => { setDateTime(new Date()) }
+    useEffect(() => {
+        setInterval(() => tick(),1000);
+    }, [])
+  return <h2>{dateTime.toLocaleTimeString()}</h2>
+} 
+
 export const Footer = () => {
-    return (
-    <footer>
-        Footer
-    </footer>
-    )
+    return (<footer>
+        <Name name="City" />
+        <Time />
+    </footer>)
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Time, Name} from './App';
-import {Footer} from './components/Footer';
+import { Footer } from './components/Footer';
 
 const rootElement = document.getElementById('root');
 
-const render = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Name name="pine grove" />
-      <Time time={new Date().toLocaleTimeString()} />
-      <Footer />
-    </React.StrictMode>,
-    rootElement
-  );
-}
-
-setInterval(render, 1000);
-
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>,
+  rootElement
+);
