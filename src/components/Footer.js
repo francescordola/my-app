32bd4f81f58cd1bc8ef7dc99ce5a615171abed57
Francesco Cordola <francesco.cordola@outlook.com>
2021-09-12 18:24:43 +0200

    12/09 - update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 
-//CITY
-const Name = (props) => {
-    //console.log('city')
-    const name = <h1>{props.name}</h1> 
-    return name
-}
-
 /*  
 dentro alla funzione con useState scrivo quello che succede in background
 quello che stampo lo gestisco al di fuori della funzione con gli Hooks
-*/
 
-// const DateFormatted = ({ date }) => <h2>{date.toLocaleDateString()}</h2>
-// const TimeFormatted = ({ date }) => <h2>{date.toLocaleTimeString()}</h2>
+ex.*/
 
 // DATE + TIME
 const DateTime = () => {
@@ -34,20 +25,29 @@ const DateTime = () => {
     const time = <h2>{date.toLocaleTimeString()}</h2>
     const data = <h2>{date.toLocaleDateString()}</h2>
 
+    // const DateFormatted = ({ date }) => <h2>{date.toLocaleDateString()}</h2>
+    // const TimeFormatted = ({ date }) => <h2>{date.toLocaleTimeString()}</h2>
+
     return <>
         {data}
         {time}
 
         {/* <DateFormatted date={date} /> 
         <TimeFormatted date={date} /> */}
-        
     </>
 } 
+
+// CITY
+const Name = (props) => {
+    //console.log('city')
+    const name = <h1>{props.name}</h1> 
+    return name
+}
 
 export const Footer = () => {
     return (
     <footer>
-        <Name name='City' />
+        <Name name='geolocation' />
         <DateTime />
     </footer>)
 }
