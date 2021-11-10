0b5cadec4462de074580bfcbd91c589246766a81
Francesco Cordola <francesco.cordola@outlook.com>
2021-11-10 14:52:46 +0100

    buttons (vito)

--- src/components/Buttons/buttons.jsx
+++ src/components/Buttons/buttons.jsx
@@ -2,24 +2,52 @@ import React from "react";
 import { useState } from "react";
 import { Button } from "./Button/button";
 
+
+// VITO
+const buttons = [
+    { label: 'How to read' },
+    { label: 'About' }
+]
+
 const Buttons = () => {
-  const [color, setColor] = useState("red");
+  const [index, setIndex] = useState(0);
 
   return (
-    <div>
-        {/* cosa succede quando clicco sul bottone, come si comporta il componente? */}
-
-        {/* TEST 1 */}
-        <Button onClick={() => { setColor("black"); }} style={{ background: color }} name="Button" /> 
+      <div>
     
-        {/* Vito */}
-         {/* 
-        <Button onClick={() => props.onClick(0)} active={true} name="Button"/>
-        <Button onClick={() => props.onClick(1)} active={true} name="Button"/>  
-        */}
-          
+          {buttons.map((button, i) =>
+          {
+              return <Button onClick={() => setIndex(i)} active={index === i} name={button.label} />
+          })}
+                
     </div>
   );
 };
 
+// TEST 2
+// const Buttons = () => {
+    
+//     const [index, setIndex] = useState(0);
+
+//     return (
+//         <div>
+//             <Button onClick={() => setIndex(0)} active={index === 0} name='button0' />
+//             <Button onClick={() => setIndex(1)} active={index === 1} name='button1' />
+//         </div>
+//     )
+// };
+
+// TEST 1
+// const Buttons = () => {
+//     const [color, setColor] = useState("red");
+        
+//   return (
+//       <div>
+          
+//         <Button onClick={() => { setColor("black"); }} style={{ background: color }} name="Button" /> 
+     
+//         </div>
+//         );
+//     };     
+       
 export default Buttons;
